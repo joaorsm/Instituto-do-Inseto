5e3458fbd0d42976d6f51b952eafe3607c5e1ca1
Bruno Tufanin Prinzo <brunoprinzo@hotmail.com>
2020-08-20 02:41:35 -0300

    regiao

---
+++ Regiao.java
@@ -0,0 +1,17 @@
+public class Regiao{
+    private String pais;
+    private String estado;
+
+    public String getPais(){
+        return pais;
+    }
+    public String getEstado(){
+        return estado;
+    }
+    public void setPais(String pais){
+        this.pais = pais;
+    }
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+}
