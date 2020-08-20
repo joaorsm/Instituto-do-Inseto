7f93d336c9354ddb44f904472f1b385315b2213e
joaorsm <joao_rsm@hotmail.com>
2020-08-20 03:39:15 -0300

    teste 1

--- Run.java
+++ Run.java
@@ -7,6 +7,21 @@ public class Run
 		Leitura leitura = new Leitura();
 		Aracnideos aracnideo =  new Aracnideos();
 
-		aracnideo.setQuantidadeOlhos(Integer.parseInt(leitura.entDados("Entre com a quantidade de olhos: ")));		
+		aracnideo.setQuantidadeOlhos(Integer.parseInt(leitura.entDados("Entre com a quantidade de olhos: ")));
+
+		aracnideo.setPeconhento(Boolean.parseBoolean(leitura.entDados("Peconhento? (true or false): ")));
+
+		aracnideo.setCor(leitura.entDados("Entre com a cor: "));
+
+		aracnideo.getRegiao().setPais(leitura.entDados("Entre com o pais de origem: "));
+
+		aracnideo.getRegiao().setEstado(leitura.entDados("Entre com o estado de origem: "));
+
+		System.out.println("Pernas: "+ aracnideo.getPernas());
+		System.out.println("Olhos: "+ aracnideo.getQuantidadeOlhos());
+		System.out.println("Peconhento: "+ aracnideo.getPeconhento());
+		System.out.println("Cor: "+ aracnideo.getCor());
+		System.out.println("Pais: "+ aracnideo.getRegiao().getPais());
+		System.out.println("Estadp: "+ aracnideo.getRegiao().getEstado());
 	}
 }
