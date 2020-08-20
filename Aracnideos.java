6a5a0e9e825c36b6b888b2d42b26c2a65d4bc716
joaorsm <joao_rsm@hotmail.com>
2020-08-20 02:49:28 -0300

    Update Aracnideos.java

--- Aracnideos.java
+++ Aracnideos.java
@@ -1,7 +1,60 @@
+//João Roberto Savini Merante RA 1886525
+
 public class Aracnideos
 {
-	public static void main(String[] args)
+	private int pernas = 8;
+	private int quantidadeOlhos;
+	private boolean peconhento;
+	private String cor;
+	private Regiao regiao = new Regiao();
+
+	public int getPernas()
 	{
-		GFDLJDFGLKFDSHJDFSLKFDSAF
+		return pernas;
+	}
+
+	public void setPernas(int pernas)
+	{
+		this.pernas = pernas;
+	}
+
+	public int getQuantidadeOlhos()
+	{
+		return quantidadeOlhos;
+	}
+
+	public void setQuantidadeOlhos(int quantidadeOlhos)
+	{
+		this.quantidadeOlhos = quantidadeOlhos;
+	}
+
+	public boolean getPeconhento()
+	{
+		return peconhento;
+	}
+
+	public void setPeconhento(boolean peconhento)
+	{
+		this.peconhento = peconhento;
+	}
+
+	public String getCor()
+	{
+		return cor;
+	}
+
+	public void setCor(String cor)
+	{
+		this.cor = cor;
+	}
+
+	public Regiao getRegiao()
+	{
+		return regiao;
+	}
+
+	public void setRegiao(Regiao regiao)
+	{
+		this.regiao = regiao;
 	}
 }
